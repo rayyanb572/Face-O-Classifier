c9e8627fb1e088ae0e5089e394559b25b54a5209
rayyanb572 <baihaqi572@gmail.com>
2025-08-22 22:01:34 +0700

    update config

--- config.py
+++ config.py
@@ -1,19 +1,28 @@
 import hashlib
+import os
+import sys
+from dotenv import load_dotenv
+
+# Load env
+load_dotenv()
 
 # --- Admin Configuration ---
-ADMIN_USERNAME = "admin"
-ADMIN_PASSWORD = hashlib.sha256("admin123".encode()).hexdigest()  # Default password: admin123
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
+ADMIN_PASSWORD_RAW = os.getenv("ADMIN_PASSWORD")
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+# Hash password
+ADMIN_PASSWORD = hashlib.sha256(ADMIN_PASSWORD_RAW.encode()).hexdigest()
 
 # --- Flask Configuration ---
-SECRET_KEY = "your_secret_key"
-SESSION_TYPE = 'filesystem'
-SESSION_FILE_DIR = 'flask_session'
-SESSION_PERMANENT = False
+SESSION_TYPE = os.getenv("SESSION_TYPE", "filesystem")
+SESSION_FILE_DIR = os.getenv("SESSION_FILE_DIR", "flask_session")
+SESSION_PERMANENT = os.getenv("SESSION_PERMANENT", "False").lower() == "true"
 
 # --- Folder Configuration ---
-UPLOAD_FOLDER = 'uploads'
-ZIP_FOLDER = 'zip'
-DATABASE_FOLDER = 'database'
+UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", "uploads")
+ZIP_FOLDER = os.getenv("ZIP_FOLDER", "zip")
+DATABASE_FOLDER = os.getenv("DATABASE_FOLDER", "database")
 
 # --- File Extensions ---
 ALLOWED_IMAGE_EXTENSIONS = ['.jpg', '.jpeg', '.png']
